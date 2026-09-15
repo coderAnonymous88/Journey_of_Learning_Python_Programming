159b8d59482b63f04ec4c6a655be468299e1b035
AWAIS MIRZA <mawais245gb@gmail.com>
2026-09-15 21:04:29 +0500

    Initial commit

---
+++ DAY 70/hierarical_inheritance.py
@@ -0,0 +1,10 @@
+class BaseClass:
+    pass
+
+class Derived1(BaseClass):
+    pass
+
+class Derived2(Derived1):
+    pass
+
+# This is the Hierarical Inhertance
---
+++ DAY 70/hybrid_inheritance.py
@@ -0,0 +1,14 @@
+class BaseClass:
+    pass
+
+class Derived1(BaseClass):
+    pass
+
+class Derived2(Derived1):
+    pass
+
+class Derived3(Derived1 , Derived2):
+    pass
+
+
+# This is the Hybrid Inhertance
