713835b95aa81b150b76142e0a87f60c24807796
AWAIS MIRZA <mawais245gb@gmail.com>
2026-09-21 18:41:34 +0500

    Initial commit

---
+++ DAY 74/warlas_operator.py
@@ -0,0 +1,25 @@
+# a = True
+# print(a:=False)
+
+
+
+# numbers = [1, 2 ,3 ,4 ,5]
+
+# while(n:= len(numbers)) > 0:
+#     print(numbers.pop())
+
+
+
+# singers = list()
+
+# while True:
+#     singer = input("Enter Your favourite singer name: ")
+#     if singer == "quit":
+#         break
+#     singers.append(singer)
+
+
+
+singers = list()
+while(singer:= input("Enter your favourinte singer name : ")) != "quit":
+    singers.append(singer)
